9119cdca1d5cf11be282a3c3ce50cc58d951e794
Werner Keil <werner.keil@gmx.net>
2014-06-19 19:08:41 +0200

    5: Improve demos
    Task-Url:
    https://github.com/unitsofmeasurement/unit-demos/issues/issue/5

--- src/main/java/org/unitsofmeasurement/ri/util/SI.java
+++ src/main/java/org/unitsofmeasurement/ri/util/SI.java
@@ -1,10 +1,17 @@
-/*
- * JScience - Java(TM) Tools and Libraries for the Advancement of Sciences.
- * Copyright (C) 2010-2013 - JScience (http://jscience.org/)
- * All rights reserved.
+/**
+ *  Unit-API - Units of Measurement API for Java
+ *  Copyright 2010-2014, Jean-Marie Dautelle, Werner Keil, V2COM and individual
+ *  contributors by the @author tag.
  *
- * Permission to use, copy, modify, and distribute this software is
- * freely granted, provided that this notice is preserved.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.unitsofmeasurement.ri.util;
 
@@ -34,10 +41,11 @@ import org.unitsofmeasurement.ri.model.QuantityDimension;
  * @see <a href="http://en.wikipedia.org/wiki/International_System_of_Units">Wikipedia: International System of Units</a>
  * @see <a href="http://physics.nist.gov/cuu/Units/outside.html>Units outside the SI that are accepted for use with the SI</a>
  * @see <a href="http://www.bipm.org/utils/common/pdf/si_brochure_8.pdf>SI 2006 - Official Specification</a>
- * @see SIPrefixOld
+ * @see SIPrefix
  *
  * @author  <a href="mailto:jean-marie@dautelle.com">Jean-Marie Dautelle</a>
- * @version 5.1, November 17, 2013
+ * @author  <a href="mailto:units@catmedia.us">Werner Keil</a>
+ * @version 0.5.1, June 19, 2014
 */
 public final class SI extends AbstractSystemOfUnits {
 
