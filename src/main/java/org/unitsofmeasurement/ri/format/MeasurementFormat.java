fddd5be9fde7d87b1ac1acffe0fcdc4d43aab41f
Werner Keil <werner.keil@gmx.net>
2014-08-12 01:04:04 +0200

    UNITSOFMEASUREMENT-13: Do we need AbstractMeasurement in RI?
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-13

--- src/main/java/org/unitsofmeasurement/ri/format/MeasurementFormat.java
+++ src/main/java/org/unitsofmeasurement/ri/format/MeasurementFormat.java
@@ -53,12 +53,6 @@ public abstract class MeasurementFormat extends Format implements Parser<CharSeq
 	private static final long serialVersionUID = -4628006924354248662L;
 
 	/**
-	 * Holds the default format instance.
-	 */
-	private static final ValueSpaceUnit DEFAULT = new ValueSpaceUnit(
-			NumberFormat.getInstance(), LocalUnitFormat.getInstance());
-
-	/**
 	 * Holds the standard format instance.
 	 */
 	private static final Standard STANDARD = new Standard();
@@ -71,7 +65,7 @@ public abstract class MeasurementFormat extends Format implements Parser<CharSeq
 	 * @return <code>MeasureFormat.getInstance(NumberFormat.getInstance(), UnitFormat.getInstance())</code>
 	 */
 	public static MeasurementFormat getInstance() {
-		return DEFAULT;
+		return STANDARD;
 	}
 
 //	/**
@@ -101,12 +95,8 @@ public abstract class MeasurementFormat extends Format implements Parser<CharSeq
 	 */
 	public static MeasurementFormat getInstance(FormatBehavior style) {
 		switch (style) {
-		case LOCALE_NEUTRAL:
-			return STANDARD;
-		case LOCALE_SENSITIVE:
-			return DEFAULT;
 		default:
-			return DEFAULT;
+			return STANDARD;
 		}
 	}
 
@@ -233,64 +223,6 @@ public abstract class MeasurementFormat extends Format implements Parser<CharSeq
 		} catch (IOException ex) {
 			throw new RuntimeException(ex); // Should not happen.
 		}
-	}
-
-	// Holds default implementation.
-	private static final class ValueSpaceUnit extends MeasurementFormat {
-
-		private final NumberFormat _numberFormat;
-
-		private final UnitFormat _unitFormat;
-
-		private ValueSpaceUnit(NumberFormat numberFormat, UnitFormat unitFormat) {
-			_numberFormat = numberFormat;
-			_unitFormat = unitFormat;
-		}
-
-		@Override
-		public Appendable format(Measurement<?, ?> measurement, Appendable dest)
-				throws IOException {
-//			Unit unit = measure.getUnit();
-//			if (unit instanceof CompoundUnit)
-//				return formatCompound(measure.doubleValue(unit),
-//						(CompoundUnit) unit, dest);
-//			else {
-//				dest.append(_numberFormat.format(measure.getValue()));
-//				if (measure.getUnit().equals(SI.ONE))
-//					return dest;
-//				dest.append(' ');
-//				return _unitFormat.format(measure.getUnit(), dest);
-				dest.append(String.valueOf(measurement));
-				return dest;
-		}
-
-		@SuppressWarnings("unchecked")
-		public Measurement parse(CharSequence csq, ParsePosition cursor)
-				throws IllegalArgumentException, ParserException {
-			String str = csq.toString();
-			Number number = _numberFormat.parse(str, cursor);
-			if (number == null)
-				throw new IllegalArgumentException("Number cannot be parsed");
-			Unit unit = _unitFormat.parse(csq);
-//			if (number instanceof BigDecimal)
-//				return AbstractMeasurement.of((BigDecimal) number, unit);
-			if (number instanceof Long)
-				return AbstractMeasurement.of(((Long) number).longValue(), unit);
-//			else if (number instanceof Double)
-//				return AbstractMeasurement.of(number.doubleValue(), unit);
-			else if (number instanceof Integer)
-				return AbstractMeasurement.of(number.intValue(), unit);
-			else
-				throw new UnsupportedOperationException("Number of type "
-						+ number.getClass() + " are not supported");
-		}
-		
-		public Measurement parse(CharSequence csq) throws IllegalArgumentException, ParserException {
-			return parse(csq, new ParsePosition(0));
-		}
-
-		private static final long serialVersionUID = 1L;
-
 	}
 
 	// Holds standard implementation.
