932138ad5b9da244c5a98165e7b1f3a758adc55e
Werner Keil <werner.keil@gmx.net>
2014-12-10 00:25:37 +0100

    UNITSOFMEASUREMENT-25: Adjust spec document to recent code changes
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-25

--- src/main/java/tec/units/ri/AbstractUnit.java
+++ src/main/java/tec/units/ri/AbstractUnit.java
@@ -1,6 +1,6 @@
 /**
- *  Unit-API - Units of Measurement API for Java
- *  Copyright (c) 2005-2014, Jean-Marie Dautelle, Werner Keil, V2COM.
+ * Unit-API - Units of Measurement API for Java
+ * Copyright (c) 2005-2014, Jean-Marie Dautelle, Werner Keil, V2COM.
  *
  * All rights reserved.
  *
