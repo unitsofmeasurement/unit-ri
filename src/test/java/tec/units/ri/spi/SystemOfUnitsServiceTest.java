ddebd52fb400eebe60e2d8af83368a6aee3ce3ce
Werner Keil <werner.keil@gmx.net>
2015-07-24 00:59:19 +0200

    Move Bootstrap mechanism into API
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-145

---
+++ src/test/java/tec/units/ri/spi/SystemOfUnitsServiceTest.java
@@ -0,0 +1,33 @@
+/**
+ * Unit-API - Units of Measurement API for Java
+ * Copyright (c) 2014 Jean-Marie Dautelle, Werner Keil, V2COM
+ * All rights reserved.
+ *
+ * See LICENSE.txt for details.
+ */
+package tec.units.ri.spi;
+
+import static org.junit.Assert.assertNull;
+
+import javax.measure.spi.SystemOfUnitsService;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+@Ignore
+public class SystemOfUnitsServiceTest {
+
+    private SystemOfUnitsService metric;
+
+    @Before
+    public void setUp() {
+//      metric = Metric.getInstance();
+    }
+
+    @Test
+    public void testGetUnits() {
+        assertNull(metric);
+    }
+}
