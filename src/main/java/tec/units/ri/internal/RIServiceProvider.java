95c5e8e8378b63842d5de343e9c150f0eecf7523
Werner Keil <werner.keil@gmx.net>
2015-07-24 23:45:39 +0200

    77: Move Bootstrap mechanism into API
    Task-Url:
    https://github.com/unitsofmeasurement/uom-se/issues/issues/issue/77

--- src/main/java/tec/units/ri/internal/RIServiceProvider.java
+++ src/main/java/tec/units/ri/internal/RIServiceProvider.java
@@ -23,7 +23,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package tec.units.ri.spi;
+package tec.units.ri.internal;
 
 import java.util.ArrayList;
 import java.util.Collections;
