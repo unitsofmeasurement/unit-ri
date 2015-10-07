ebcc3d7dc4dc19905ff032f868cd5cea375721b4
Werner Keil <werner.keil@gmx.net>
2015-10-07 23:56:27 +0200

    UNITSOFMEASUREMENT-164: Change license header
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-164

--- src/main/java/tec/units/ri/spi/Measurement.java
+++ src/main/java/tec/units/ri/spi/Measurement.java
@@ -1,6 +1,6 @@
 /*
- *  Unit-API - Units of Measurement API for Java
- *  Copyright (c) 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
+ * Units of Measurement Reference Implementation
+ * Copyright © 2005-2015, Jean-Marie Dautelle, Werner Keil, V2COM.
  *
  * All rights reserved.
  *
