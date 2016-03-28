3ed4829558346c2d59d3bb891996e7b1028bd277
Werner Keil <werner.keil@gmx.net>
2016-03-29 01:18:29 +0200

    26: Default System Service on RI returned, not CommonSystemService
    Task-Url:
    https://github.com/unitsofmeasurement/uom-systems/issues/issues/issue/26

--- src/main/java/tec/units/ri/internal/RIServiceProvider.java
+++ src/main/java/tec/units/ri/internal/RIServiceProvider.java
@@ -29,7 +29,7 @@
  */
 package tec.units.ri.internal;
 
-import static java.util.logging.Level.WARNING;
+import static java.util.logging.Level.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -47,136 +47,152 @@ import javax.measure.spi.ServiceProvider;
 import tec.uom.lib.common.function.IntPrioritySupplier;
 
 /**
- * This class implements the {@link ServiceProvider} interface and hereby uses the JDK {@link java.util.ServiceLoader} to load the services required.
+ * This class implements the {@link ServiceProvider} interface and hereby uses
+ * the JDK {@link java.util.ServiceLoader} to load the services required.
  *
  * @author Werner Keil
- * @version 0.3
+ * @version 0.4
  */
 public class RIServiceProvider implements ServiceProvider, IntPrioritySupplier {
-  /** List of services loaded, per class. */
-  @SuppressWarnings("rawtypes")
-  private final Map<Class, List<Object>> servicesLoaded = new HashMap<Class, List<Object>>();
+    /** List of services loaded, per class. */
+    @SuppressWarnings("rawtypes")
+    private final Map<Class, List<Object>> servicesLoaded = new HashMap<Class, List<Object>>();
 
-  static final class ServiceCompare implements Comparator<Object> {
-    @Override
-    public int compare(Object o1, Object o2) {
-      System.out.println("Comparing " + o1 + " and " + o2);
-      int prio1 = 0;
-      int prio2 = 0;
+    static final class ServiceCompare implements Comparator<Object> {
+	@Override
+	public int compare(Object o1, Object o2) {
+	    Logger.getLogger(RIServiceProvider.class.getName()).log(INFO,
+		    "Comparing " + o1 + " and " + o2);
+	    int prio1 = 0;
+	    int prio2 = 0;
 
-      if (o1 instanceof IntPrioritySupplier) {
-        prio1 = ((IntPrioritySupplier) o1).getPriority();
-      }
-      if (o2 instanceof IntPrioritySupplier) {
-        prio2 = ((IntPrioritySupplier) o2).getPriority();
-      }
-      if (prio1 < prio2) {
-        return 1;
-      }
-      if (prio2 < prio1) {
-        return -1;
-      }
-      return o2.getClass().getName().compareTo(o1.getClass().getName()); // TODO maybe use something else here?
+	    if (o1 instanceof IntPrioritySupplier) {
+		prio1 = ((IntPrioritySupplier) o1).getPriority();
+	    }
+	    if (o2 instanceof IntPrioritySupplier) {
+		prio2 = ((IntPrioritySupplier) o2).getPriority();
+	    }
+	    if (prio1 < prio2) {
+		return 1;
+	    }
+	    if (prio2 < prio1) {
+		return -1;
+	    }
+	    return o2.getClass().getName().compareTo(o1.getClass().getName()); // TODO
+									       // maybe
+									       // use
+									       // something
+									       // else
+									       // here?
+	}
     }
-  }
 
-  private static final Comparator<Object> SERVICE_COMPARATOR = new ServiceCompare();
+    private static final Comparator<Object> SERVICE_COMPARATOR = new ServiceCompare();
 
-  public int getPriority() {
-    return 10;
-  }
+    public int getPriority() {
+	return 10;
+    }
 
-  /**
-   * Loads and registers services.
-   *
-   * @param serviceType
-   *          The service type.
-   * @param <T>
-   *          the concrete type.
-   * @return the items found, never {@code null}.
-   */
-  public <T> List<T> getServices(final Class<T> serviceType) {
+    /**
+     * Loads and registers services.
+     *
+     * @param serviceType
+     *            The service type.
+     * @param <T>
+     *            the concrete type.
+     * @return the items found, never {@code null}.
+     */
+    public <T> List<T> getServices(final Class<T> serviceType) {
+	@SuppressWarnings("unchecked")
+	List<T> found = (List<T>) servicesLoaded.get(serviceType);
+	if (found != null) {
+	    return found;
+	}
+
+	return loadServices(serviceType);
+    }
+
+    public <T> T getService(Class<T> serviceType) {
+	List<T> servicesFound = getServices(serviceType);
+	if (servicesFound.isEmpty()) {
+	    return null;
+	}
+	return servicesFound.get(0);
+    }
+
+    /**
+     * Loads and registers services.
+     *
+     * @param serviceType
+     *            The service type.
+     * @param <T>
+     *            the concrete type.
+     *
+     * @return the items found, never {@code null}.
+     */
     @SuppressWarnings("unchecked")
-    List<T> found = (List<T>) servicesLoaded.get(serviceType);
-    if (found != null) {
-      return found;
+    private <T> List<T> loadServices(final Class<T> serviceType) {
+	final List<T> services = new ArrayList<T>();
+	try {
+	    for (T t : ServiceLoader.load(serviceType)) {
+		services.add(t);
+	    }
+	    Collections.sort(services, SERVICE_COMPARATOR);
+	    final List<T> previousServices = (List<T>) servicesLoaded.put(
+		    serviceType, (List<Object>) services);
+	    return list(previousServices != null ? previousServices.iterator()
+		    : services.iterator());
+	} catch (Exception e) {
+	    Logger.getLogger(RIServiceProvider.class.getName()).log(WARNING,
+		    "Error loading services of type " + serviceType, e);
+	    Collections.sort(services, SERVICE_COMPARATOR);
+	    return services;
+	}
     }
 
-    return loadServices(serviceType);
-  }
-
-  public <T> T getService(Class<T> serviceType) {
-    List<T> servicesFound = getServices(serviceType);
-    if (servicesFound.isEmpty()) {
-      return null;
+    @Override
+    public int compareTo(ServiceProvider o) {
+	return compare(getPriority(), o.getPriority());
     }
-    return servicesFound.get(0);
-  }
 
-  /**
-   * Loads and registers services.
-   *
-   * @param serviceType
-   *          The service type.
-   * @param <T>
-   *          the concrete type.
-   *
-   * @return the items found, never {@code null}.
-   */
-  @SuppressWarnings("unchecked")
-  private <T> List<T> loadServices(final Class<T> serviceType) {
-    final List<T> services = new ArrayList<T>();
-    try {
-      for (T t : ServiceLoader.load(serviceType)) {
-        services.add(t);
-      }
-      Collections.sort(services, SERVICE_COMPARATOR);
-      final List<T> previousServices = (List<T>) servicesLoaded.put(serviceType, (List<Object>) services);
-      return list(previousServices != null ? previousServices.iterator() : services.iterator());
-    } catch (Exception e) {
-      Logger.getLogger(RIServiceProvider.class.getName()).log(WARNING, "Error loading services of type " + serviceType, e);
-      Collections.sort(services, SERVICE_COMPARATOR);
-      return services;
+    /**
+     * Compares two {@code int} values numerically. The value returned is
+     * identical to what would be returned by:
+     * 
+     * <pre>
+     * Integer.valueOf(x).compareTo(Integer.valueOf(y))
+     * </pre>
+     *
+     * @param x
+     *            the first {@code int} to compare
+     * @param y
+     *            the second {@code int} to compare
+     * @return the value {@code 0} if {@code x == y}; a value less than
+     *         {@code 0} if {@code x < y}; and a value greater than {@code 0} if
+     *         {@code x > y}
+     */
+    private static int compare(int x, int y) {
+	return (x < y) ? -1 : ((x == y) ? 0 : 1);
     }
-  }
 
-  @Override
-  public int compareTo(ServiceProvider o) {
-    return compare(getPriority(), o.getPriority());
-  }
-
-  /**
-   * Compares two {@code int} values numerically. The value returned is identical to what would be returned by:
-   * 
-   * <pre>
-   * Integer.valueOf(x).compareTo(Integer.valueOf(y))
-   * </pre>
-   *
-   * @param x
-   *          the first {@code int} to compare
-   * @param y
-   *          the second {@code int} to compare
-   * @return the value {@code 0} if {@code x == y}; a value less than {@code 0} if {@code x < y}; and a value greater than {@code 0} if {@code x > y}
-   */
-  private static int compare(int x, int y) {
-    return (x < y) ? -1 : ((x == y) ? 0 : 1);
-  }
-
-  /**
-   * Returns an array list containing the elements returned by the specified iterator in the order they are returned by the enumeration. This method
-   * provides interoperability between legacy APIs that return enumerations and new APIs that require collections.
-   *
-   * @param e
-   *          enumeration providing elements for the returned array list
-   * @return an array list containing the elements returned by the specified enumeration.
-   * @since 1.4
-   * @see Enumeration
-   * @see ArrayList
-   */
-  private static <T> ArrayList<T> list(Iterator<T> i) {
-    ArrayList<T> l = new ArrayList<>();
-    while (i.hasNext())
-      l.add(i.next());
-    return l;
-  }
+    /**
+     * Returns an array list containing the elements returned by the specified
+     * iterator in the order they are returned by the enumeration. This method
+     * provides interoperability between legacy APIs that return enumerations
+     * and new APIs that require collections.
+     *
+     * @param e
+     *            enumeration providing elements for the returned array list
+     * @return an array list containing the elements returned by the specified
+     *         enumeration.
+     * @since 1.4
+     * @see Enumeration
+     * @see ArrayList
+     */
+    private static <T> ArrayList<T> list(Iterator<T> i) {
+	ArrayList<T> l = new ArrayList<>();
+	while (i.hasNext())
+	    l.add(i.next());
+	return l;
+    }
 }
