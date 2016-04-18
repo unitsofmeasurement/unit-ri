2fffe3739056e1dd3282d4336913bfb8ddf8133e
Werner Keil <werner.keil@gmx.net>
2016-04-19 00:06:50 +0200

    UNITSOFMEASUREMENT-192: Short number support for Quantities
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-192

---
+++ src/test/java/tec/units/ri/quantity/AdhocRadianTest.java
@@ -0,0 +1,69 @@
+/*
+ * Units of Measurement Reference Implementation
+ * Copyright (c) 2005-2016, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.units.ri.quantity;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.measure.Quantity;
+import javax.measure.Unit;
+import javax.measure.quantity.Angle;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tec.units.ri.function.AddConverter;
+import tec.units.ri.function.PiMultiplierConverter;
+import tec.units.ri.function.RationalConverter;
+import tec.units.ri.unit.TransformedUnit;
+import tec.units.ri.unit.Units;
+
+public class AdhocRadianTest {
+  private Unit<Angle> adhocRads;
+
+  @Before
+  public void setUp() throws Exception {
+    adhocRads = new TransformedUnit<>(Units.RADIAN, new AddConverter(0.00004793773773).concatenate(new PiMultiplierConverter()
+        .concatenate(new RationalConverter(2, 65535))));
+  }
+
+  @Test
+  public void testWithInteger() {
+    Quantity<Angle> minusPiRadian = Quantities.getQuantity(-Math.PI, Units.RADIAN);
+    Quantity<Angle> leftEnd = Quantities.getQuantity(-32768, adhocRads);
+    assertEquals(minusPiRadian.getValue().doubleValue(), leftEnd.to(Units.RADIAN).getValue().doubleValue(), 0.0000001);
+  }
+
+  @Test
+  public void testWithShort() {
+    Quantity<Angle> minusPiRadian = Quantities.getQuantity(-Math.PI, Units.RADIAN);
+    Quantity<Angle> leftEnd = Quantities.getQuantity(Short.MIN_VALUE, adhocRads);
+    assertEquals(minusPiRadian.getValue().doubleValue(), leftEnd.to(Units.RADIAN).getValue().doubleValue(), 0.0000001);
+  }
+}
