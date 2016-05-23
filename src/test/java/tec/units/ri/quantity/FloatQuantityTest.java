335a041912ccf0d986741f976c74b70ceb418c7f
Werner Keil <werner.keil@gmx.net>
2016-05-23 19:52:03 +0200

    UNITSOFMEASUREMENT-180: Code Coverage
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-180

---
+++ src/test/java/tec/units/ri/quantity/FloatQuantityTest.java
@@ -0,0 +1,91 @@
+/*
+ * Units of Measurement Reference Implementation
+ * Copyright (c) 2005-2016, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.units.ri.quantity;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.ElectricResistance;
+import javax.measure.quantity.Length;
+import javax.measure.quantity.Time;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import tec.units.ri.quantity.Quantities;
+import tec.units.ri.unit.Units;
+
+public class FloatQuantityTest {
+
+  @Test
+  public void divideTest() {
+    FloatQuantity<ElectricResistance> quantity1 = new FloatQuantity<ElectricResistance>(Float.valueOf(3).floatValue(), Units.OHM);
+    FloatQuantity<ElectricResistance> quantity2 = new FloatQuantity<ElectricResistance>(Float.valueOf(2).floatValue(), Units.OHM);
+    Quantity<?> result = quantity1.divide(quantity2);
+    assertEquals(Float.valueOf(1.5f), result.getValue());
+  }
+
+  @Test
+  public void addTest() {
+    FloatQuantity quantity1 = new FloatQuantity(Float.valueOf(1).floatValue(), Units.OHM);
+    FloatQuantity quantity2 = new FloatQuantity(Float.valueOf(2).floatValue(), Units.OHM);
+    Quantity<ElectricResistance> result = quantity1.add(quantity2);
+    assertEquals(Float.valueOf(3f), result.getValue());
+  }
+
+  @Test
+  public void subtractTest() {
+    FloatQuantity<ElectricResistance> quantity1 = new FloatQuantity<ElectricResistance>(Float.valueOf(1).floatValue(), Units.OHM);
+    FloatQuantity<ElectricResistance> quantity2 = new FloatQuantity<ElectricResistance>(Float.valueOf(2).floatValue(), Units.OHM);
+    Quantity<ElectricResistance> result = quantity2.subtract(quantity1);
+    assertEquals(Float.valueOf(1), result.getValue());
+    assertEquals(Units.OHM, result.getUnit());
+  }
+
+  @Test
+  public void multiplyQuantityTest() {
+    FloatQuantity<ElectricResistance> quantity1 = new FloatQuantity<ElectricResistance>(Float.valueOf(3).floatValue(), Units.OHM);
+    FloatQuantity<ElectricResistance> quantity2 = new FloatQuantity<ElectricResistance>(Float.valueOf(2).floatValue(), Units.OHM);
+    Quantity<?> result = quantity1.multiply(quantity2);
+    assertEquals(Float.valueOf(6L), result.getValue());
+  }
+
+  @Test
+  public void toTest() {
+    Quantity<Time> day = Quantities.getQuantity(1D, Units.DAY);
+    Quantity<Time> hour = day.to(Units.HOUR);
+    Assert.assertEquals(Double.valueOf(24), hour.getValue());
+    Assert.assertEquals(hour.getUnit(), Units.HOUR);
+
+    Quantity<Time> dayResult = hour.to(Units.DAY);
+    Assert.assertEquals(dayResult.getValue(), day.getValue());
+    Assert.assertEquals(dayResult.getUnit(), day.getUnit());
+  }
+}
---
+++ src/test/java/tec/units/ri/quantity/IntegerQuantityTest.java
@@ -0,0 +1,89 @@
+/*
+ * Units of Measurement Reference Implementation
+ * Copyright (c) 2005-2016, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.units.ri.quantity;
+
+import static org.junit.Assert.assertEquals;
+import javax.measure.Quantity;
+import javax.measure.quantity.ElectricResistance;
+import javax.measure.quantity.Time;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import tec.units.ri.quantity.Quantities;
+import tec.units.ri.unit.Units;
+
+public class IntegerQuantityTest {
+
+  @Test
+  public void divideTest() {
+    IntegerQuantity<ElectricResistance> quantity1 = new IntegerQuantity<ElectricResistance>(Long.valueOf(3).intValue(), Units.OHM);
+    IntegerQuantity<ElectricResistance> quantity2 = new IntegerQuantity<ElectricResistance>(Long.valueOf(2).intValue(), Units.OHM);
+    Quantity<?> result = quantity1.divide(quantity2);
+    assertEquals(Double.valueOf(1.5d), result.getValue());
+  }
+
+  @Test
+  public void addTest() {
+    IntegerQuantity quantity1 = new IntegerQuantity(Long.valueOf(1).intValue(), Units.OHM);
+    IntegerQuantity quantity2 = new IntegerQuantity(Long.valueOf(2).intValue(), Units.OHM);
+    Quantity<ElectricResistance> result = quantity1.add(quantity2);
+    assertEquals(Short.valueOf("3").intValue(), result.getValue().intValue());
+  }
+
+  @Test
+  public void subtractTest() {
+    IntegerQuantity<ElectricResistance> quantity1 = new IntegerQuantity<ElectricResistance>(Long.valueOf(1).intValue(), Units.OHM);
+    IntegerQuantity<ElectricResistance> quantity2 = new IntegerQuantity<ElectricResistance>(Long.valueOf(2).intValue(), Units.OHM);
+    Quantity<ElectricResistance> result = quantity2.subtract(quantity1);
+    assertEquals(Short.valueOf("1").intValue(), result.getValue().intValue());
+    assertEquals(Units.OHM, result.getUnit());
+  }
+
+  @Test
+  public void multiplyQuantityTest() {
+    IntegerQuantity<ElectricResistance> quantity1 = new IntegerQuantity<ElectricResistance>(Long.valueOf(3).intValue(), Units.OHM);
+    IntegerQuantity<ElectricResistance> quantity2 = new IntegerQuantity<ElectricResistance>(Long.valueOf(2).intValue(), Units.OHM);
+    Quantity<?> result = quantity1.multiply(quantity2);
+    assertEquals(Integer.valueOf(6), result.getValue());
+  }
+
+  @Test
+  public void toTest() {
+    Quantity<Time> day = Quantities.getQuantity(1D, Units.DAY);
+    Quantity<Time> hour = day.to(Units.HOUR);
+    Assert.assertEquals(hour.getValue().intValue(), 24);
+    Assert.assertEquals(hour.getUnit(), Units.HOUR);
+
+    Quantity<Time> dayResult = hour.to(Units.DAY);
+    Assert.assertEquals(dayResult.getValue().intValue(), day.getValue().intValue());
+    Assert.assertEquals(dayResult.getValue().intValue(), day.getValue().intValue());
+  }
+}
