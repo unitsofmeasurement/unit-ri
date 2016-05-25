91a4f7eeb7e6901f4d0225171c68435db4231b23
Werner Keil <werner.keil@gmx.net>
2016-05-25 23:51:00 +0200

    UNITSOFMEASUREMENT-180: Code Coverage
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-180

---
+++ src/test/java/tec/units/ri/internal/format/l10n/DigitListTest.java
@@ -0,0 +1,69 @@
+/*
+ * Units of Measurement Reference Implementation
+ * Copyright (c) 2005-2016, Jean-Marie Dautelle, Werner Keil, V2COM.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ *    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of JSR-363 nor the names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package tec.units.ri.internal.format.l10n;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DigitListTest {
+
+  static DigitList sut;
+
+  @BeforeClass
+  public static void init() {
+    sut = new DigitList();
+  }
+
+  @Test
+  public void testGetDouble() {
+    assertEquals(0d, sut.getDouble(), 0);
+    assertEquals(0L, sut.getLong(), 0);
+  }
+
+  @Test
+  public void testIsZero() {
+    assertTrue(sut.isZero());
+  }
+  
+  @Test
+  public void testToString() {
+    assertEquals("0", sut.toString());
+  }
+  
+  @Test
+  public void testSet() {
+	  DigitList list = new DigitList();
+	  list.set(1);
+	  assertEquals(1d, list.getDouble(), 0);
+	  assertEquals(1L, list.getLong(), 0);
+  }
+}
