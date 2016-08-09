4be4ecee586e183086a88d302b0f16d464b31378
Werner Keil <werner.keil@gmx.net>
2016-08-09 22:22:26 +0200

    UNITSOFMEASUREMENT-194: Prepare Final
    Task-Url: https://java.net/jira/browse/UNITSOFMEASUREMENT-194

--- src/main/java/tec/units/ri/function/NaturalOrder.java
+++ src/main/java/tec/units/ri/function/NaturalOrder.java
@@ -36,6 +36,10 @@ import javax.measure.Quantity;
 /**
  * Comparator to sort by natural order, looking both the unit and the value.
  * 
+ * @author <a href="mailto:werner@uom.technology">Werner Keil</a>
+ * @author <a href="mailto:otaviopolianasantana@gmail.com">Otavio Santana</a>
+ * @version 1.0
+ * 
  * @return <b>Given:</b>
  *         <p>
  *         Quantity<Time> day = Quantities.getQuantity(1, Units.DAY);
